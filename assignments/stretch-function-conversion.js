8fef2a5298a75ba782ba7d287cd93eca5b105382
Timothy Shores <timothymshores@gmail.com>
2019-01-22 17:53:39 -0600

    Completed strech problems on strech-function-conversion.js

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -6,18 +6,31 @@
 //   return param;
 // };
 
+let myArrowFunction = param => param;
+console.log(myArrowFunction('Argument'));
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let addArrow = (param1, param2) => param1 + param2;
+console.log(addArrow(1, 2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-// exampleArray = [1,2,3,4];
+let subtractArrow = (param1, param2) => param1 - param2;
+console.log(subtractArrow(1, 2));
+
+// exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1, 2, 3, 4];
+let tripleArrow = array => array.map(element => element * 3);
+console.log(tripleArrow(exampleArray));
